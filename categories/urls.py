0900be99016c8cff8d6ab922c5d56dd2a01f25f6
eskokado <eskokado@gmail.com>
2023-04-27 10:14:28 -0300

    feat: Create category retrieve, update and destroy view

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from categories.views import CategoryListCreateAPIView
+from categories.views import CategoryListCreateAPIView, CategoryRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path("categories/", CategoryListCreateAPIView.as_view(), name="categories")
+    path("categories/", CategoryListCreateAPIView.as_view(), name="categories-list-create"),
+    path("categories/<int:pk>/", CategoryRetrieveUpdateDestroyAPIView.as_view(), name="category-retrieve-update-destroy"),
 ]
--- categories/views.py
+++ categories/views.py
@@ -8,3 +8,8 @@ from categories.serializers import CategorySerializer
 class CategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
