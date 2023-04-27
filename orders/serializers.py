c9aaf2aa80c87a88ea039177bc1b31c34b006df7
eskokado <eskokado@gmail.com>
2023-04-27 08:29:14 -0300

    fix: add OrderItemSerializer in the OrderSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from orders.models import Order
+from orders_items.serializers import OrderItemSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
         fields = "__all__"
