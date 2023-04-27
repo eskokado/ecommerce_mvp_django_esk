3fc42e2c0955e925f253f6fb0f49a0db5ca817b2
eskokado <eskokado@gmail.com>
2023-04-27 07:32:36 -0300

    feat: create model Order

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    shipping_address = models.TextField()
+    paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order {self.id} - User {self.user.username}"
