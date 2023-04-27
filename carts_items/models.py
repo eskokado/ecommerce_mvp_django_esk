ba8965f6440b82c9556fd546cbdd4f9df4ba6fe1
eskokado <eskokado@gmail.com>
2023-04-27 07:24:18 -0300

    feat: create model CartItem

--- carts_items/models.py
+++ carts_items/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from carts.models import Cart
+from products.models import Product
+
+
+class CartItem(models.Model):
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cart {self.cart.id} - Item {self.id}"
