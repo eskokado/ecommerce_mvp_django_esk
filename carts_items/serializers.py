bfe8a2a29e27b3787f21fb7f879c73a51df65379
eskokado <eskokado@gmail.com>
2023-04-28 06:18:16 -0300

    feat: add ProductSerializer and field product in the CartItemSerializer

--- carts_items/serializers.py
+++ carts_items/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
 from carts_items.models import CartItem
+from products.models import Product
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'category')
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    cart = serializers.PrimaryKeyRelatedField(read_only=True)
+    product = ProductListSerializer(read_only=True)
+
     class Meta:
         model = CartItem
-        fields = "__all__"
+        fields = ('id', 'cart', 'quantity', 'unit_price', 'product')
