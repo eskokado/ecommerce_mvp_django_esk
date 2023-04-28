22dc7b07ecd74617ac7b615a78d97ff47915a90b
eskokado <eskokado@gmail.com>
2023-04-28 07:37:50 -0300

    feat: add user in the OrderSerializer and fix method create

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,12 +1,38 @@
 from rest_framework import serializers
 
+from carts.serializers import UserSerializer
 from orders.models import Order
+from orders_items.models import OrderItem
 from orders_items.serializers import OrderItemSerializer
+from products.models import Product
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True)
+    items = OrderItemSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = ('id', 'created_at', 'updated_at', 'user', 'items')
+
+    def create(self, validated_data):
+        items_data = self.context['request'].data.get('items')
+        order = Order.objects.create(**validated_data)
+
+        for item_data in items_data:
+            product_id = item_data.get('product')
+            product = Product.objects.get(id=product_id)
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=item_data.get('quantity'),
+                unit_price=item_data.get('unit_price')
+            )
+
+        return order
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        items = instance.orderitem_set.all()
+        representation['items'] = OrderItemSerializer(items, many=True).data
+        return representation
