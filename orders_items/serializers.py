6906fca635afdf8d9a303bb8ea30a32d5864a760
eskokado <eskokado@gmail.com>
2023-04-28 07:36:51 -0300

    feat: add product in the OrderItemSerializer

--- orders_items/serializers.py
+++ orders_items/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from carts_items.serializers import ProductListSerializer
 from orders_items.models import OrderItem
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    order = serializers.PrimaryKeyRelatedField(read_only=True)
+    product = ProductListSerializer(read_only=True)
+
     class Meta:
         model = OrderItem
-        fields = "__all__"
+        fields = ('id', 'order', 'quantity', 'unit_price', 'product')
+
