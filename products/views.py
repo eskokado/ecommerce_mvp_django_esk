5fdbb102a8dea50ddf78e1924596043a53625743
eskokado <eskokado@gmail.com>
2023-04-28 04:37:29 -0300

    feat: Create ProductRetrieveUpdateDestroyAPIView

--- products/views.py
+++ products/views.py
@@ -7,3 +7,8 @@ from products.serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
