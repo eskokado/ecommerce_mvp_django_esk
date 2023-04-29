2ac6f5cfb6f0ea02d5046dbf65935164ce29ef94
eskokado <eskokado@gmail.com>
2023-04-29 06:14:44 -0300

    feat: implement permissions in the view products

--- products/views.py
+++ products/views.py
@@ -2,13 +2,16 @@ from rest_framework import generics
 
 from products.models import Product
 from products.serializers import ProductSerializer
+from users.permissions import IsAuthenticatedAndAdminOrSafeMethodsAndAuthenticated
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedAndAdminOrSafeMethodsAndAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedAndAdminOrSafeMethodsAndAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
